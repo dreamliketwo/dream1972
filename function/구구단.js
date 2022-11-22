83d7a696eb4f57b1219a34eda2656e83b6be8429
dreamliketwo <02dreamlike@naver.com>
2022-11-22 20:42:58 +0900

    구구단

---
+++ function/구구단.js
@@ -0,0 +1,13 @@
+function 구구단(몇단) {
+    console.log(몇단 * 1)
+    console.log(몇단 * 2)
+    console.log(몇단 * 3)
+    console.log(몇단 * 4)
+    console.log(몇단 * 5)
+    console.log(몇단 * 6)
+    console.log(몇단 * 7)
+    console.log(몇단 * 8)
+    console.log(몇단 * 9)
+}
+구구단(4);
+구구단(9);
